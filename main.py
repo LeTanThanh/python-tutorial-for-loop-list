a14ba08f0f7b2f1c3ac59b54870b0dce1a4d605a
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-30 20:16:04 +0700

    Using Python for loop to iterate over a list with index

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ if __name__ == "__main__":
   cities = ["New York", "Beijing", "Cairo", "Mumbai", "Mexico"]
   for city in cities:
     print(city)
+
+  # Using Python for loop to iterate over a list with index
+
+  cities = ["New York", "Beijing", "Cairo", "Mumbai", "Mexico"]
+
+  for item in enumerate(cities):
+    print(item)
+
+  for index, city in enumerate(cities):
+    print(f"{index}: {city}")
+
+  for index, city in enumerate(cities, 1):
+    print(f"{index}: {city}")
